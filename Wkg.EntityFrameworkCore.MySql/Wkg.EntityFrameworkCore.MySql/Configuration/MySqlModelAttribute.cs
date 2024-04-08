d33d3683c548bf9a733910853812f73de34e88a2
Frederik Höft <hoeft@wkg-software.de>
2024-04-08 17:37:13 +0200

    version 8.1

--- Wkg.EntityFrameworkCore.MySql/Wkg.EntityFrameworkCore.MySql/Configuration/MySqlModelAttribute.cs
+++ Wkg.EntityFrameworkCore.MySql/Wkg.EntityFrameworkCore.MySql/Configuration/MySqlModelAttribute.cs
@@ -1,8 +1,5 @@
 ﻿using Wkg.EntityFrameworkCore.Configuration.Reflection.Attributes;
-using Wkg.EntityFrameworkCore.Extensions;
-using Microsoft.EntityFrameworkCore;
-using Wkg.EntityFrameworkCore.Configuration.Policies.NamingPolicies;
-using Wkg.EntityFrameworkCore.Configuration.Policies.MappingPolicies;
+using Wkg.EntityFrameworkCore.Configuration.Reflection.Discovery;
 
 namespace Wkg.EntityFrameworkCore.MySql.Configuration;
 
@@ -10,6 +7,6 @@ namespace Wkg.EntityFrameworkCore.MySql.Configuration;
 /// Indicates that the decorated class should be loaded by the MySQL configuration loader, if multiple configuration loaders are used by the application.
 /// </summary>
 /// <remarks>
-/// The attribute on its own does not do anything. It must be passed to the <see cref="ModelBuilderExtensions.LoadReflectiveModels{TDatabaseEngineModelAttribute}(ModelBuilder, INamingPolicy?, IMappingPolicy?)"/> method to be effective.
+/// The attribute on its own does not do anything. It must be passed as the generic type parameter to <see cref="IDiscoveryOptionsBuilder.AddTargetDatabaseEngine{TTargetEngine}()"/> to be effective.
 /// </remarks>
 public sealed class MySqlModelAttribute : DatabaseEngineModelAttribute;
--- Wkg.EntityFrameworkCore.MySql/Wkg.EntityFrameworkCore.MySql/ProcedureMapping/Configuration/Reflection/MySqlDiscoveryOptionsBuilder.cs
+++ Wkg.EntityFrameworkCore.MySql/Wkg.EntityFrameworkCore.MySql/ProcedureMapping/Configuration/Reflection/MySqlDiscoveryOptionsBuilder.cs
@@ -2,4 +2,8 @@
 
 namespace Wkg.EntityFrameworkCore.MySql.ProcedureMapping.Configuration.Reflection;
 
-internal class MySqlDiscoveryOptionsBuilder : DiscoveryOptionsBuilder;
+internal class MySqlDiscoveryOptionsBuilder : DiscoveryOptionsBuilder
+{
+    public override IDiscoveryOptionsBuilder AddTargetDatabaseEngine<TTargetEngine>() => 
+        throw new NotSupportedException("Multiple database engine support is not available for stored procedures. The target database engine is automatically determined through the implemented interfaces.");
+}
