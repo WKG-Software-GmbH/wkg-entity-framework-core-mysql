91dc014d7247c5b10d2b1a12e646cfe88dcec094
Frederik Höft <hoeft@wkg-software.de>
2024-07-10 15:56:44 +0200

    update deps and add version info

---
+++ Wkg.EntityFrameworkCore.MySql/Wkg.EntityFrameworkCore.MySql/WkgEntityFrameworkCoreMySql.cs
@@ -0,0 +1,25 @@
+﻿using Wkg.Versioning;
+
+namespace Wkg.EntityFrameworkCore.MySql;
+
+/// <summary>
+/// Provides version information for the Wkg.EntityFrameworkCore.MySql framework.
+/// </summary>
+public class WkgEntityFrameworkCoreMySql : DeploymentVersionInfo
+{
+    private const string __CI_DEPLOYMENT_VERSION_PREFIX = "0.0.0";
+    private const string __CI_DEPLOYMENT_VERSION_SUFFIX = "CI-INJECTED";
+    private const string __CI_DEPLOYMENT_DATETIME_UTC = "1970-01-01 00:00:00";
+
+    private WkgEntityFrameworkCoreMySql() : base
+    (
+        __CI_DEPLOYMENT_VERSION_PREFIX,
+        __CI_DEPLOYMENT_VERSION_SUFFIX,
+        __CI_DEPLOYMENT_DATETIME_UTC
+    ) => Pass();
+
+    /// <summary>
+    /// Provides version information for the Wkg.EntityFrameworkCore.MySql framework.
+    /// </summary>
+    public static WkgEntityFrameworkCoreMySql VersionInfo { get; } = new WkgEntityFrameworkCoreMySql();
+}
